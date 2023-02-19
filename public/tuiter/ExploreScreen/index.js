553d91654280885eee51250ab56c7fb7bdc8165f
Xiaojin Cai <xcai0511@gmail.com>
2023-02-19 18:02:35 -0500

    a5 JavaScript fall 23

--- public/tuiter/ExploreScreen/index.js
+++ public/tuiter/ExploreScreen/index.js
@@ -6,15 +6,12 @@ function exploreComponent() {
     $('#wd-explore').append(`
       <div class="row mt-2">
        <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-        <!--<h3>Navigation Sidebar</h3>-->
         ${NavigationSidebar('Explore')}
        </div>
        <div class="col-10 col-lg-7 col-xl-6">
-        <!--<h3>ExploreComponent</h3>-->
         ${ExploreComponent()}
        </div>
        <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-        <!--<h3>WhoToFollowList </h3>-->
         ${WhoToFollowList()}
        </div>
       </div>
