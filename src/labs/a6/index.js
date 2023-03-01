20db816cbbfd13500c876dceb1f9f36f75302d9e
Xiaojin Cai <xcai0511@gmail.com>
2023-03-01 15:58:47 -0500

    a5 JavaScript fall 23

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,14 +1,20 @@
-import {Link}
-    from "react-router-dom";
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
+import TodoItem from "./todo/todo-item";
+import TodoList from "./todo/todo-list";
 
 function Assignment6() {
-    return(
+    return (
         <div>
-            <Link to="/">Lab</Link> |
-            <Link to="/hello">Hello</Link> |
-            <Link to="/tuiter">Tuiter</Link>
             <h1>Assignment 6</h1>
+            <TodoList/>
+            <h3>Todo Item</h3>
+            <TodoItem/>
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
         </div>
-    )
+    );
 }
 export default Assignment6;
