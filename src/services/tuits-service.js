3f4225632750f78e26a717763533abc96e1723fb
Xiaojin Cai <xcai0511@gmail.com>
2023-03-30 21:02:02 -0400

    a8 REST sp23

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_BASE;
+console.log(API_BASE);
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
